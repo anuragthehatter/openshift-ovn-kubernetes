c0b72ba02f8a3cca8bf510e9b3d6918983e8baa8
Nadia Pinaeva <n.m.pinaeva@gmail.com>
2025-09-16 14:52:43 +0200

    Add transit subnet conflict handling for Layer2 networks.
    Add a transitSubnet field similar to joinSubnet to the NetConf,
    but only set it for Primary Layer2 networks.
    Set transit subnets for NADs
    
    Signed-off-by: Nadia Pinaeva <n.m.pinaeva@gmail.com>
    (cherry picked from commit 62bd1ebbdd458a969dcb60e0d68da0784ac61273)

--- go-controller/pkg/config/config.go
+++ go-controller/pkg/config/config.go
@@ -2507,7 +2507,7 @@ func completeConfig() error {
 		return err
 	}
 
-	if err := allSubnets.CheckForOverlaps(); err != nil {
+	if _, _, err := allSubnets.CheckForOverlaps(); err != nil {
 		return err
 	}
 
